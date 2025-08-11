b24e13d80d5d5854ba8084593b7ec221e89b2e70
Chirleispi <chirlei_spinelli@hotmail.com>
2025-08-11 16:47:58 -0300

    Update 1-variaveis.py

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,12 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Chirlei'
+profissão = 'Química'
 # Declare variáveis de cada tipo numérico
-
+idade = 41
+salário = 6500,44
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
